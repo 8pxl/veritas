302255c8b287a844233effd7e9c413b5d3bedb51
8pxl <56054380+8pxl@users.noreply.github.com>
2026-02-22 04:51:01 -0500

    pretty landing page

--- frontend/app/components/home/hero.tsx
+++ frontend/app/components/home/hero.tsx
@@ -1,13 +1,31 @@
 export default function Hero() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <div className="font-borel text-9xl">
-        verit<span className="text-[#BF5864]">ai</span>s
+    <div className="w-screen h-screen relative flex flex-col items-center justify-center overflow-hidden">
+
+      <video
+        className="rounded-2xl absolute inset-0 m-auto w-[80%] pointer-events-none select-none"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src="/landing.mp4" type="video/mp4" />
+      </video>
+
+      {/* Text Content */}
+      <div className="bg-black/30 w-[80%] aspect-video flex items-center text-center justify-center backdrop-opacity-50 rounded-2xl py-12 px-10">
+        <div>
+          <div className="relative z-10 font-borel text-white text-9xl">
+            verit<span className="text-[#BF5864]">ai</span>s
+          </div>
+
+          <div className="relative z-10 text-xl text-white">
+            finding <span className="text-[#BF5864] font-bold">truth</span> in a world where <span className="text-[#BF5864] font-bold">deception</span> is money
+          </div>
+        </div>
       </div>
-      <div className="text-xl">
-        finding <span className="text-[#BF5864] font-bold">truth</span> in a world where <span className="text-[#BF5864] font-bold">deception</span> is money
-      </div>
-    </div >
+
+    </div>
   )
 
 }
