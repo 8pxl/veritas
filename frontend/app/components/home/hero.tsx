a49f717031a8cfba61283226c06b56e825e3069a
8pxl <56054380+8pxl@users.noreply.github.com>
2026-02-21 22:37:58 -0500

    change name

--- frontend/app/components/home/hero.tsx
+++ frontend/app/components/home/hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       <div className="font-borel text-9xl">
-        veritas
+        verit<span className="text-[#BF5864]">ai</span>s
       </div>
       <div className="text-xl">
         finding <span className="text-[#BF5864] font-bold">truth</span> in a world where <span className="text-[#BF5864] font-bold">deception</span> is money
